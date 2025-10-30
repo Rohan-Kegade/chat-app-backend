f60110a13e6cbeb3c8c4057bcf4675ade07ba9f2
Rohan Kegade <rohankegade123@gmail.com>
2025-10-30 21:58:38 +0530

    added asyncHandler

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import type { Request, Response, NextFunction, RequestHandler } from "express";
+
+type AsyncHandler = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) => RequestHandler;
+
+export const asyncHandler: AsyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
