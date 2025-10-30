1a75f77fb66922cd056aea94dff26c9603dd0590
Rohan Kegade <rohankegade123@gmail.com>
2025-10-30 20:42:02 +0530

    added db connection logic and updated start commands

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log("Connected to Database.");
+  } catch (err: any) {
+    console.log(`Databse connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
-
+import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+connectDB();
 
 app.use("/", (req, res) => {
   res.send("Hello World!");
